57b5c200a1ec182d52a1b561182d6abf8ba3f8ac
SMGyo <alsryaks1@naver.com>
2025-01-10 16:39:35 +0900

    스프링 첫 푸시

---
+++ demo/src/main/java/com/example/demo/mapper/EmployeesMapper.java
@@ -0,0 +1,12 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface EmployeesMapper {
+	List<EmployeesDto> findAll();
+	EmployeesDto findById(long id);
+	List<EmployeesDto> findBydeptAndName(@Param("departmentId") Long departmentId, @Param("firstName") String firstName);
+}
---
+++ demo/src/test/java/com/example/demo/MapTest.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public class MapTest {
+	
+	@Test
+	public void test() {
+		// List (ArrayList , LinkedList) : 순차 { 1,2,1,4,6}
+		// Set : 중복값X 
+		// Map<key, Value> : 검색 , VO 대신 맵 사용가능
+		
+		Map<String,Object> emp = new HashMap<>();
+		emp.put("firstName", "scott");
+		emp.put("lastName", "king");
+		
+		System.out.println(emp.get("firstName"));
+		System.out.println(emp.get("lastName"));
+	}
+}
